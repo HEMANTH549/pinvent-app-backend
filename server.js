0ee25b7aaf71255116d40d8899cae9f119205336
HEMANTH549 <tumaradahemanthofficial@gmail.com>
2023-04-14 21:20:03 +0530

    initial commit

--- server.js
+++ server.js
@@ -3,34 +3,48 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
 const userRoute = require("./routes/userRoute");
-const errorHandler = require("./middleware/errorMiddleware");
-//Middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-//Route Middleware
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+        credentials: true,
+    })
+);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes Middleware
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
 
-//Routes
+// Routes
 app.get("/", (req, res) => {
-    res.send("Home Page!!");
+    res.send("Home Page");
 });
 
-
-//Error Middleware
-app.use(errorHandler)
-
-
-//Connect to DB and start server 
+// Error Middleware
+app.use(errorHandler);
+// Connect to DB and start server
 const PORT = process.env.PORT || 5000;
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`server running on port ${PORT}`);
+            console.log(`Server Running on port ${PORT}`);
         });
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
